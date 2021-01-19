1957068a84a9cb09f5b9929b7486c361e0b5cd6e
nathanRomad <nathan.a.hamilton@gmail.com>
2021-01-19 15:36:11 -0600

    final data entered for cityDataProvider

--- scripts/antarctica/cities/CityDataProvider.js
+++ scripts/antarctica/cities/CityDataProvider.js
@@ -1,19 +1,29 @@
 const cityCollection = [
     {
-        image: "../../../images/images_Antarctica/",
-        alt: "test",
-        name: "test",
+        image: "../../../images/images_Antarctica/rossIslandMap.png",
+        alt: "A map showing Ross Island, Antarctica.",
+        name: "Ross Island",
     },  
     {
-        image: "",
-        alt: "",
-        name: "",
+        image: "../../../images/images_Antarctica/dryValley.jpg",
+        alt: "A view of the Dry Valleys from a small science field camp.",
+        name: "Dry Valleys",
     },  
     {
-        image: "",
-        alt: "",
-        name: "",
-    },    
+        image: "../../../images/images_Antarctica/southPole.jpg",
+        alt: "With the United States South Pole Station in the background, and flags planted surrounding a pole set into the ice. The Ceremonial South Pole is an area set aside for photo opportunities at the South Pole Station. It is re-located each year to account for the is shift and represents the Geographic South Pole, and consists of a metallic sphere on a short barber pole, surrounded by the flags of the original Antarctic Treaty signatory states.",
+        name: "South Pole",
+    },
+    {
+        image: "../../../images/images_Antarctica/pineIslandGlacier.jpg",
+        alt: "Satelite view of a massive glacier seperating from the Antarctic continent.",
+        name: "Western Antarctica",
+    },
+    {
+        image: "../../../images/images_Antarctica/antarcticaPeninsula.jpg",
+        alt: "A overhead view of the entire Antarctic continent",
+        name: "Peninsula",
+    },
 ]
 
 export const useCities = () => {
