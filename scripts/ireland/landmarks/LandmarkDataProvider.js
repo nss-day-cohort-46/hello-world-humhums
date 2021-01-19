0953ec4ad2d090b194c2026f61d28c155a6eb77d
bpotts11 <brittneympotts@gmail.com>
2021-01-19 16:12:46 -0600

    just typed layout of data provider for landmarks

---
+++ scripts/ireland/landmarks/LandmarkDataProvider.js
@@ -0,0 +1,31 @@
+const landmarkCollection = [
+    {
+        image: "",
+        alt: "",
+        name: "",
+    },
+    {
+        image: "",
+        alt: "",
+        name: "",
+    },
+    {
+        image: "",
+        alt: "",
+        name: "",
+    },
+    {
+        image: "",
+        alt: "",
+        name: "",
+    },
+    {
+        image: "",
+        alt: "",
+        name: "",
+    },
+]
+
+export const useLandmarks = () => {
+    return landmarkCollection.slice()
+}
