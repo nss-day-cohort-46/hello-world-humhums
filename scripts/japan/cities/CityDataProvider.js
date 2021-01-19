8a1212b9ae7f767296c0501a5513028be4e20916
Nick Carver <nicholascarver2@outlook.com>
2021-01-19 15:42:46 -0600

    got the cities to load on my page

--- scripts/japan/cities/CityDataProvider.js
+++ scripts/japan/cities/CityDataProvider.js
@@ -1,4 +1,3 @@
-// cold hard raw fish data. aka sushi.
 const cityCollection = [
     {
         name: "Tokyo",
@@ -6,24 +5,24 @@ const cityCollection = [
         alt: "a helicopter view of the city of Tokyo at sunrise with blur at the bottom"
     },
     {
-        name: "",
-        image: "",
-        alt: ""
+        name: "Osaka",
+        image: "https://s3.voyapon.com/wp-content/uploads/2020/04/22172053/Osaka_Shinsekai_86.jpg",
+        alt: "downtown streets of osaka at night where the signs light up the sidewalks"
     },
     {
-        name: "",
-        image: "",
-        alt: ""
+        name: "Kyoto",
+        image: "https://cdn.contexttravel.com/image/upload/c_fill,q_60,w_2600/v1553227874/production/city/hero_image_27_1553227874.jpg",
+        alt: "a residential street view of Kyoto facing a temple"
     },
     {
-        name: "",
-        image: "",
-        alt: ""
+        name: "Sapporo",
+        image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/4d/45/dc/sapporo.jpg?w=1100&h=600&s=1",
+        alt: "the city of Sopporo covered in snow at the base of the mountain"
     },
     {
-        name: "",
-        image: "",
-        alt: ""
+        name: "Yokohama",
+        image: "https://lp-cms-production.imgix.net/2019-06/fa6b8fcce2932f0b7b04d383035192ff-yokohama.jpg",
+        alt: "the skyline of Yokohama from the river with a ferris wheel lit in pink lights"
     },
 ]
 
