71a1c42505fa8d00d660ce924499f249159446ba
powellmad <madsnpowell@gmail.com>
2021-01-19 15:54:35 -0600

    added some city data

--- scripts/czech/cities/CityDataProvider.js
+++ scripts/czech/cities/CityDataProvider.js
@@ -1,13 +1,13 @@
 const cityCollection = [
     {
         image: "../../../images/images_czech/cesky-krumlov.jpg",
-        alt: "Cesky",
+        alt: "Cesky Krumlov, a preserved medieval town with red roofs, green trees, and a blue river surrounding it",
         name: "Cesky Krumlov"
     },
     {
-        image: "",
-        alt: "",
-        name: ""
+        image: "../../../images/images_czech/prague.jpg",
+        alt: "Prauge street with statues and the famous tunnel at sunset",
+        name: "Prauge"
     },
     {
         image: "",
