5b4bdfaab61a5b344ccfa2e23a503a82c5433ca0
bpotts11 <brittneympotts@gmail.com>
2021-01-19 20:04:00 -0600

    finished setting up and automating famous citizens for Ireland. I added a headline in the h2. I deleted the no longer relevant html

---
+++ scripts/ireland/citizens/CitizenList.js
@@ -0,0 +1,28 @@
+import { Citizen } from "./Citizen.js"
+import { useCitizens } from "./CitizenDataProvider.js"
+
+let citizenHTML = ""
+const buildCitizenHTML = (citizenArray) => {
+    for (const citizenObj of citizenArray) {
+        citizenHTML += Citizen(citizenObj)
+
+    }
+    return citizenHTML
+}
+
+export const CitizenList = () => {
+    const contentElement = document.querySelector(".contentContainer")
+
+    const allCitizensArray = useCitizens()
+
+    buildCitizenHTML(allCitizensArray)
+
+    contentElement.innerHTML += `
+    <article class="contentContainer__citizens">
+    <h3>Famous Citizens</h3>
+    <div class="contentContainer__cards">
+        ${citizenHTML}
+    </div>
+    </article>  
+    `
+}
--- scripts/ireland/main.js
+++ scripts/ireland/main.js
@@ -1,3 +1,14 @@
 import { CityList } from "./cities/CityList.js"
-
 CityList()
+
+
+
+
+
+
+
+
+
+
+import { CitizenList } from "./citizens/CitizenList.js"
+CitizenList()
