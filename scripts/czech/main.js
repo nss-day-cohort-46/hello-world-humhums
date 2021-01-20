415816fd8d3a70797c1494c88b975548faf26b76
powellmad <madsnpowell@gmail.com>
2021-01-20 10:31:39 -0600

    had to add LandmarkList.js on this branch

--- scripts/czech/main.js
+++ scripts/czech/main.js
@@ -2,6 +2,6 @@ import { CityList } from "./cities/CityList.js"
 import { LandmarkList } from "./landmarks/LandmarkList.js"
 import { CitizenList } from "./citizens/CitizenList.js"
 
-CitizenList()
 CityList()
 LandmarkList()
+CitizenList()
